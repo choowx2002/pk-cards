4ad26a737a8aed7aa668c9055214c565eac48985
choowx2002 <85860769+choowx2002@users.noreply.github.com>
2025-03-07 15:10:02 +0800

    add createWebHashHistory

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import CardView from "@/views/CardView.vue";
 import CardList from "@/views/CardList.vue";
@@ -21,7 +21,7 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes,
 });
 
